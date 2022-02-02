08e2462a2be5b9d6c01f467b5c3b128c69ff65ca
kristijanbosnjak <kristijan.bosnjak@tronic.digital>
2022-02-02 13:48:32 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React deploy
+          Learn React deployeeed
         </a>
       </header>
     </div>
