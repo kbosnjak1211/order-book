cd21f55bd04d4df25c12ccca7c6ec3f225d1c497
kristijanbosnjak <kristijan.bosnjak@tronic.digital>
2022-02-02 13:37:08 +0100

    change deply and text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React cccccc
+          Learn React deploy
         </a>
       </header>
     </div>
